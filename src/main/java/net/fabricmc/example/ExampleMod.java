2d830eea709a1fc646808ac9afc3f3d1326d34a2
Straybbit <straybbitdev@gmail.com>
2023-01-25 10:37:07 +0900

    Refiller 하다 말았음

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,9 +1,11 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +18,12 @@ public class ExampleMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+
+	//Items_________________________________________
+	public static final Item STEAMPUNK_ENGINE = new Item(new Item.Settings());
+
+	
+	//Blocks________________________________________
 	public static final Block REFILLER = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
 
 	@Override
@@ -23,6 +31,7 @@ public class ExampleMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
 
 		LOGGER.info("Hello Fabric world!");
 
